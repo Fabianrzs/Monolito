7182f8f0e96d0305adc4257f05c1441485b029dd
Fabian Ramirez <fabianramirez0072@gmail.com>
2024-02-29 22:41:50 -0500

    Create Models note

---
+++ WebApi/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Descriptions { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
